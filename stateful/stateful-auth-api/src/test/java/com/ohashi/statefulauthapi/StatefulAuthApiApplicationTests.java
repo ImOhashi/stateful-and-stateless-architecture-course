fcf456378b7315fb0d1ef88d3c51b56f1b6cb28a
ImOhashi <leonardoohashi.mkt@gmail.com>
2024-04-05 21:54:29 -0300

    first commit with spring projects

---
+++ stateful/stateful-auth-api/src/test/java/com/ohashi/statefulauthapi/StatefulAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohashi.statefulauthapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatefulAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ stateless/stateless-auth-api/src/main/java/com/ohashi/statelessauthapi/StatelessAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.ohashi.statelessauthapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatelessAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatelessAuthApiApplication.class, args);
+	}
+
+}
